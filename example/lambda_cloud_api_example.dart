f5fd1ef1d4969fb4f6702c7e180911b2a2efe1ec
Wesley Moy <wesley.moy@gmail.com>
2025-08-20 11:24:01 -0400

    Empty project
        dart create -t package lambda-cloud-api

---
+++ example/lambda_cloud_api_example.dart
@@ -0,0 +1,6 @@
+import 'package:lambda_cloud_api/lambda_cloud_api.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/lambda_cloud_api_test.dart
@@ -0,0 +1,16 @@
+import 'package:lambda_cloud_api/lambda_cloud_api.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
